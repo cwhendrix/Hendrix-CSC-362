0b14b3aa6a3fa2f39abc7f2cd3f06efa31696617
cwhendrix <cwhendrix3@icloud.com>
2023-10-02 15:21:53 +0000

    Finished adding in data

--- lab5/lab05.sql
+++ lab5/lab05.sql
@@ -81,7 +81,7 @@ CREATE TABLE returns (
     carpet_id       INT         NOT NULL,
     customer_id     INT         NOT NULL,
     rental_date     DATETIME    NOT NULL,
-    rental_due_date DATETIME    NOT NULL,
+    rental_due_date DATETIME,
     rental_return   DATETIME
 );
 -- SHOW CREATE TABLE returns;
@@ -90,7 +90,8 @@ CREATE TABLE returns (
 INSERT INTO carpet_countries (carpet_country)
 VALUES  ('Iran'),
         ('India'),
-        ('Turkey');
+        ('Turkey'),
+        ('Persia/Iran');
 
 INSERT INTO carpet_styles (carpet_style)
 VALUES  ('Ushak'),
@@ -102,5 +103,24 @@ VALUES  ('Wool'),
         ('Silk');
 
 INSERT INTO carpets (carpet_country, carpet_style, carpet_year, carpet_material, carpet_lengthft, carpet_widthft, carpet_acq_date, carpet_purchase, carpet_markup, carpet_active)
-VALUES  ('Turkey', 'Ushak', '1925', 'Wool', 5, 7, '2017-04-06', 625, 100, TRUE),
-        ('Iran', 'Tabriz', '1910', 'Silk', 10, 14, '2017-04-06', 28000, 75, TRUE);
+VALUES  ('Turkey', 'Ushak', '1925', 'Wool', 5, 7, '2017-04-06', 625, 100, FALSE),
+        ('Iran', 'Tabriz', '1910', 'Silk', 10, 14, '2017-04-06', 28000, 75, TRUE),
+        ('India', 'Agra', '2017', 'Wool', 8, 10, '2017-06-15', 1200, 100, FALSE),
+        ('India', 'Agra', '2017', 'Wool', 4, 6, '2017-06-15', 450, 120, TRUE),
+        ('Persia/Iran', 'Tabriz', '1910', 'Silk', 10, 14, '2017-06-15', 28000, 50, TRUE);
+-- SELECT * FROM carpets;
+
+INSERT INTO customers (customer_firstname, customer_lastname, customer_address, customer_city, customer_state, customer_zip, customer_phone, customer_active)
+VALUES  ('Akira', 'Ingram', '68 Country Drive', 'Roseville', 'MI', '48066', '(926) 252-6716', TRUE),
+        ('Meredith', 'Spencer', '9044 Piper Lane', 'North Royalton', 'OH', '44133', '(817) 530-5994', TRUE),
+        ('Marco', 'Page', '747 East Harrison Lane', 'Atlanta', 'GA', '30303', '(588) 799-6535', TRUE),
+        ('Sandra', 'Page', '47 East Harrison Lane', 'Atlanta', 'GA', '30303', '(997) 697-2666', TRUE),
+        ('Gloria', 'Gomez', '78 Corona Road', 'Fullerton', 'CA', '92831', '(000) 000-0000', TRUE),
+        ('Bria', 'Le', '638 Silver Spear Ct', 'Coraopolis', 'PA', '15108', '(000) 000-0000', TRUE);
+-- SELECT * FROM customers;
+
+INSERT INTO sales (carpet_id, customer_id, sell_date, sale_price, sale_return_date)
+VALUES  (1, 5, '2017-12-14', 990, NULL),
+        (3, 6, '2017-12-24', 2400, NULL),
+        (5, 2, '2017-12-24', 40000, '2017-12-26');
+-- SELECT * FROM sales;
