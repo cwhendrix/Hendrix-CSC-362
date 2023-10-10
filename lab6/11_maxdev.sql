e4aae6d88b4addbff788f18ea51923a29fd4ec4c
cwhendrix <cwhendrix3@icloud.com>
2023-10-10 18:36:59 +0000

    Finished Lab 6

--- lab6/11_maxdev.sql
+++ lab6/11_maxdev.sql
@@ -5,9 +5,22 @@
 
   Show the maximum grade established by each developer. Each developer should appear only once.
 */
+/*
+CREATE VIEW max_ids AS
+SELECT climber_first_name, climber_last_name, MAX(grade_id) AS max_grade
+  FROM climbers
+       NATURAL JOIN developed_climbs
+       NATURAL JOIN climbs
+       LEFT JOIN climb_grades ON climbs.climb_grade = climb_grades.grade_id
+ GROUP BY climber_id;
+*/
 
-SELECT climber_first_name, climber_last_name, MAX(grade_id) FROM climbers
-NATURAL JOIN developed_climbs
-NATURAL JOIN climbs
-LEFT JOIN climb_grades ON climbs.climb_grade = climb_grades.grade_id
-GROUP BY climber_id;
+ SELECT climber_first_name, climber_last_name, grade_str
+   FROM (SELECT climber_first_name, climber_last_name, MAX(grade_id) AS max_grade
+  FROM climbers
+       NATURAL JOIN developed_climbs
+       NATURAL JOIN climbs
+       LEFT JOIN climb_grades ON climbs.climb_grade = climb_grades.grade_id
+ GROUP BY climber_id
+) AS max_ids
+        LEFT JOIN climb_grades ON max_ids.max_grade = climb_grades.grade_id;
